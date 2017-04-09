a17082583f9973b47b36da7da6a56a1a26aa00d8
Tuhin Sah <tuhinsah@gmail.com>
2017-04-09 12:19:38 -0700

    Fixed calculateprobability.py

--- Code/calculateaccuracy.py
+++ Code/calculateaccuracy.py
@@ -7,13 +7,13 @@ correct_lines = 0
 
 with open(sys.argv[1], "r") as prediction_file:
     for line in prediction_file:
-    	(dialogue, dialect) = line.split(" | ")
-    	predictions.append(dialogue)
+    	(dialogue, dialect) = line.strip().split(" | ")
+    	predictions.append(dialect)
 
 with open(sys.argv[2], "r") as answer_file:
     for line in answer_file:
-    	(dialogue, dialect) = line.split(" | ")
-    	answers.append(dialogue)
+    	(dialogue, dialect) = line.strip().split(" | ")
+    	answers.append(dialect)
 
 total_lines = len(predictions)
 
@@ -21,5 +21,5 @@ for i in range(total_lines):
 	if predictions[i] == answers[i]:
 		correct_lines += 1
 
-accuracy = correct_lines / total_lines * 100.0
+accuracy = float(correct_lines) / float(total_lines) * 100.0
 print accuracy
