18281c619f82c6618c56ec64b10277858849a243
Maithili <maithili_pannase@yahoo.com>
2017-04-24 20:33:59 -0700

    Add files via upload

--- Code/hmmdecode.py
+++ Code/hmmdecode.py
@@ -1,69 +1,192 @@
-import sys, copy, math, operator
+import sys
+import math
+from collections import defaultdict
 
-def get_matrix(hmmmodel, matrix):
-	k = hmmmodel.readline().rstrip()
-	while k != "*****":
-		line = hmmmodel.readline().rstrip()
-		matrix[k] = eval(line)
-		k = hmmmodel.readline().rstrip()
+f = open('hmmmodel.txt','rU')
 
-if __name__ == '__main__':
+transition_dict = {}
+transition_list = {}
+emission_dict = {}
+possible_tags = {}
 
-	transition_matrix = {}
-	emission_matrix = {}
+len_of_transition_dict = (int)(f.readline())
+# print len_of_transition_dict
+for i in range(0, len_of_transition_dict):
+	l = f.readline()
+	prev, tag, transition_prob = l.split("\t")
+	#print prev, tag, transition_prob
+	if prev not in transition_dict:
+		transition_dict[prev] = {}
+		transition_list[prev] = []
+	transition_dict[prev][tag] = (float)(transition_prob.strip("\n"))
+	
+	transition_list[prev].append(tag)
 
-	hmmmodel = open('hmmmodel.txt','r')
+# print transition_dict
+# print transition_list
 
-	get_matrix(hmmmodel, transition_matrix)
-	get_matrix(hmmmodel, emission_matrix)
+len_of_emission_dict = (int)(f.readline())
+# print len_of_emission_dict
+for i in range(0, len_of_emission_dict):
+	l = f.readline()
+	tag, word, emission_prob = l.split("\t")
+	#print tag, word, emission_prob
+	if tag not in emission_dict:
+		emission_dict[tag] = {}
+	emission_dict[tag][word] = (float)(emission_prob.strip("\n"))
 
-	labels = transition_matrix.keys()
-	labels.remove("q0")
+#read from model all the possible tags for each word
+for l in f:
+	#list of all possible tags for a word in the training set
+	l = l.rstrip()
+	slices = l.split(" ")
+	#print slices
+	len_of_slices = len(slices)
+	#print len_of_slices
+	w = slices[0]
+	#print w
+	ls = set()
+	for i in range(1, len_of_slices):
+		ls.add(slices[i])
+	if w not in possible_tags:
+		possible_tags[w] = {}
+	possible_tags[w] = ls
+	
+# print transition_dict
+# print "\n"
+# print emission_dict
+# print "\n"
+# print possible_tags
+# print "\n"
 
-	hmmoutput = open('hmmoutput.txt','w')
 
-	with open(sys.argv[-1]) as f:
-		for line in f:
-			words = line.strip().split(' ')
-			tags = ['' for i in range(len(words))]
-			prev_labels = {}
-			prev_labels[-1] = {}
-			prev_labels[-1]["q0"] = [0, ""]
 
-			for i, word in enumerate(words):
-				prev_labels[i] = {}
-				possible_labels = []
+output_file = open("hmmoutput.txt", "w")
 
-				for label in labels:
-					if word in emission_matrix[label].keys():
-						possible_labels.append(label)
 
-				if not possible_labels:
-					for prev_label in prev_labels[i - 1].keys():
-						for label in transition_matrix[prev_label].keys():
-							if label not in prev_labels[i].keys():
-								prev_labels[i][label] = [prev_labels[i - 1][prev_label][0] + transition_matrix[prev_label][label], prev_label]
-							else:
-								prob = prev_labels[i - 1][prev_label][0] + transition_matrix[prev_label][label]
-								if prob > prev_labels[i][label][0]:
-									prev_labels[i][label] = [prob, prev_label]
-				else:
-					for possible_label in possible_labels:
-						for prev_label in prev_labels[i - 1].keys():
-							if possible_label in transition_matrix[prev_label].keys():
-								if possible_label not in prev_labels[i].keys():
-									prev_labels[i][possible_label] = [prev_labels[i - 1][prev_label][0] + transition_matrix[prev_label][possible_label] + emission_matrix[possible_label][word], prev_label]
-								else:
-									prob = prev_labels[i - 1][prev_label][0] + transition_matrix[prev_label][possible_label] + emission_matrix[possible_label][word]
-									if prob > prev_labels[i][possible_label][0]:
-										prev_labels[i][possible_label] = [prob, prev_label]
+test_file_path = sys.argv[1]
+test_file = open(test_file_path,"rU")
 
-			tags[i] = max(prev_labels[i], key=prev_labels[i].get)
 
-			while i > 0:
-				i = i - 1
-				tags[i] = prev_labels[i + 1][tags[i + 1]][1]
+for line in test_file:
+	line = line.rstrip() # each sentence in the test file
+		
+	viterbi_probability = {}
+	poss_tag_list = defaultdict(list)
+	backptr = {}
+	poss_tag_list[0] = {"q0"}
+	viterbi_probability[0, "q0"] = 0.0 #viterbi_probability[t, state] = prob
+	# print poss_tag_list
+	# print viterbi_prob 
+	# print "\n"
+	words = line.split(" ")
+	#print words
+	prev_tags = poss_tag_list[0]
+	# print prev_tags
 
-			for i in range(len(words)):
-				hmmoutput.write(words[i] + '/' + tags[i] + ' ')
-			hmmoutput.write('\n')
+	count = 1
+	for word in words:
+		#print word
+		current_tag_list = []
+		if possible_tags.has_key(word): #word is in training data
+			# print "\nPOSSIBLE TAGS: "
+			# print possible_tags[word]
+			# print "\n"
+			current_tag_list = possible_tags[word]
+			# print current_tag_list
+		else: #new word which is not present in the training set
+			# print "NOT IN TRAINING SET#############"
+			for prev_tag in prev_tags: #repeat for all previous tags
+				# print "\n"
+				# print prev_tag
+				# print transition_list[prev_tag]
+				if prev_tag in transition_list:
+					for t in transition_list[prev_tag]: 
+						#print t  
+						if t not in current_tag_list:
+						# t = transition_list[prev_tag]
+						# print t
+							if t!='qX':
+								# print t
+								current_tag_list.append(t) #add all the states to the current_tag_list which can be reached from the prev_tag
+							
+		# print current_tag_list    
+
+
+		#check each tag in the current_tag_list to select the one with maximum probaility
+		for current_tag in current_tag_list: #consider possible tags one by one
+			max_prob = float('-inf')
+			max_tag = ""
+			# print current_tag
+			for prev_tag in prev_tags:
+				# print prev_tag
+
+				if prev_tag in transition_dict:
+					if current_tag in transition_dict[prev_tag]:
+						p1 = transition_dict[prev_tag][current_tag] #p1 -> transition probability from prev_tag to current_tag
+						# print p1
+					else: p1 = transition_dict[prev_tag]["qX"]
+				# else:
+				# 	p1 = transition_dict[prev_tag]["qX"]
+
+				if current_tag in emission_dict:
+					if word in emission_dict[current_tag]:
+						p2 = emission_dict[current_tag][word] #p2 -> emission probability of the word given current_tag
+						# print p2
+					else: p2 = 1.0
+				else: p2 = 1.0 #no add-1 smoothing
+
+				p3 = viterbi_probability[count-1, prev_tag] #p3 -> max probability of the previous tag at time (t-1)
+				# print p3
+
+				prob = math.log(p1)+math.log(p2)+(float)(p3) #lof probability
+				# print prob
+				# print max_prob
+				if prob > max_prob:
+					# print("Greater!")
+					max_prob = prob
+ 					max_tag = prev_tag
+ 					#print maxprob, maxtag
+ 			
+ 			poss_tag_list[count].append(current_tag)
+			viterbi_probability[count,current_tag] = max_prob
+			backptr[count,current_tag] = max_tag
+
+		prev_tags = poss_tag_list[count] #possible tags at given time t
+		count = count+1 
+
+	# print poss_tag_list
+	# print viterbi_prob
+	# print backptr
+#------------------------------------------------------------------------------------------------------------
+
+	max_prob = float('-inf')
+	max_tag = ''
+
+
+	#find the max_tag for the last word and then go in reverse to find the max_tag for the remaining words
+	len_of_poss_tag_list = len(poss_tag_list) - 1
+	# print len_of_poss_tag_list
+	#print poss_tag_list[len_of_poss_tag_list]
+
+	for poss_tag in poss_tag_list[len_of_poss_tag_list]: #iterate through the list of possible tags of the last word
+		# print poss_tag
+		if viterbi_probability[len_of_poss_tag_list,poss_tag] > max_prob:
+			max_prob = viterbi_probability[len_of_poss_tag_list,poss_tag]
+			max_tag = poss_tag
+	# print max_tag
+
+	# print "\nTagging"
+
+	final_tag = {}
+	for x in range(len_of_poss_tag_list, 0, -1): #traverse in reverse
+		#print x
+		final_tag[words[x-1]] = max_tag
+		max_tag = backptr[x, max_tag]
+		#print maxtag
+	# print final_tag	
+
+	for word in words:
+		s = word + "/" + final_tag[word] + " "
+		output_file.write(s)
+	output_file.write("\n")
--- Code/hmmlearn.py
+++ Code/hmmlearn.py
@@ -1,198 +1,186 @@
-from __future__ import print_function
-import sys, math, codecs
-#import tensorflow as tf
-import numpy as np
+#HMM POS Tagger for Catalan
+import io
+from collections import defaultdict
+import sys
+import copy
 
-class HMM(object):
-    def __init__(self, P, initial=None):
-        self.K = P.shape[0]
+#file_path = sys.argv[1]
+#print file_path
+#lines = [line.rstrip('\n') for line in open(file_path)]
 
-        self.P = P
-        self.logP = np.log(self.P)
+transition_dict = {} #of the form: {q0: {{NN:2}. {VB,4}}....}
+emission_dict = {}
+trasition_count = {}
+emission_count = {}
+transition_prob = {}
+emission_prob = {}
+last = {}
+possible_tags_dict = {}
 
-        if initial is None:
-            self.initial = np.ones(self.K)
-            self.initial /= sum(self.initial)
-        elif len(initial) != self.K:
-            raise ValueError(
-                'dimensions of initial {} must match P[0] {}'.format(
-                    initial.shape, P.shape[0]))
+
+#input_path="hmm_train.txt"
+input_path = sys.argv[1]
+with open(input_path, "r")  as training_data:
+    for line in training_data:
+        tokens = line.split()
+        # print "\n"
+        # print(tokens)
+        prev="q0"
+        
+        #trasition_count[prev] += 1
+       
+
+        for token in tokens:
+
+            if prev not in transition_dict:
+              transition_dict[prev] = {}
+              #print "make new tag entry for prev" + prev
+
+            len_of_token=len(token)
+            #print(token)
+            #print(len_of_token)
+            word=token[0:len_of_token-3]
+            tag=token[len_of_token-2:len_of_token]
+            # print "WORD: " + word
+            # print "TAG: " + tag
+            # print "PREV: " + prev
+
+            if tag not in emission_dict:
+              emission_dict[tag] = {}
+            
+            
+
+            if trasition_count.has_key(prev):
+                trasition_count[prev] += 1 
+            else:
+                trasition_count[prev] = 1
+
+        
+            if tag in transition_dict[prev]:
+              transition_dict[prev][tag] += 1
+            else:
+              transition_dict[prev][tag] = 1
+
+
+            if word in possible_tags_dict:
+              possible_tags_dict[word].add(tag)
+            else:
+              possible_tags_dict[word] = set()
+              possible_tags_dict[word].add(tag)
+
+
+            if word in emission_dict[tag]:
+              emission_dict[tag][word] += 1
+            else:
+              emission_dict[tag][word] = 1
+
+              
+            prev = tag
+        #store the last tags
+        if last.has_key(prev):
+          last[prev]=last[prev]+1
         else:
-            self.initial = initial
-        self.loginitial = np.log(self.initial)
-
-class HMMNumpy(HMM):
-    def _viterbi_partial_forward(self, scores):
-        tmpMat = np.zeros((self.K, self.K))
-        for i in range(self.K):
-            for j in range(self.K):
-                tmpMat[i, j] = scores[i] + self.logP[i, j]
-        return tmpMat
-
-    def viterbi_decode(self, y):
-        y = np.array(y)
-
-        nT = y.shape[0]
-
-        pathStates = np.zeros((nT, self.K), dtype=np.int)
-        pathScores = np.zeros((nT, self.K))
-
-        # initialize
-        pathScores[0] = self.loginitial + np.log(y[0])
-
-        for t, yy in enumerate(y[1:]):
-            # propagate forward
-            tmpMat = self._viterbi_partial_forward(pathScores[t])
-
-            # the inferred state
-            pathStates[t + 1] = np.argmax(tmpMat, 0)
-            pathScores[t + 1] = np.max(tmpMat, 0) + np.log(yy)
-
-        # now backtrack viterbi to find states
-        s = np.zeros(nT, dtype=np.int)
-        s[-1] = np.argmax(pathScores[-1])
-        for t in range(nT - 1, 0, -1):
-            s[t - 1] = pathStates[t, s[t]]
-
-        return s, pathScores
-
-def split_line(line):
-	words_and_tags = line.strip().split(' ')
-	for i in range(len(words_and_tags)):
-		word_and_tag = words_and_tags[i].split('/')
-		tag = word_and_tag[-1]
-		if len(word_and_tag) > 2:
-			word = '/'.join(word_and_tag[:-1])
-		else:
-			word = word_and_tag[0]
-		words_and_tags[i] = [word, tag]
-#	print words_and_tags
-	return words_and_tags
-
-def smooth(matrix, col):
-	for row in matrix.keys():
-		matrix[row][col] = 1.0
-
-def add_to_transitions(matrix, row, col):
-	if row not in matrix:
-		matrix[row] = {}
-	if col not in matrix[row]:
-		smooth(matrix, col)
-	matrix[row][col] = matrix[row][col] + 1.0
-
-def add_to_emissions(matrix, row, col):
-	if row not in matrix:
-		matrix[row] = {}
-	if col not in matrix[row]:
-		matrix[row][col] = 0.0
-	matrix[row][col] = matrix[row][col] + 1.0
-
-def update_transitions(words_and_tags, transition_matrix):
-	add_to_transitions(transition_matrix, 'q0', words_and_tags[0][1])
-	for i in range(len(words_and_tags) - 1):
-		add_to_transitions(transition_matrix, words_and_tags[i][1], words_and_tags[i + 1][1])
-
-def update_emissions(words_and_tags, emission_matrix, word_set):
-	for i in range(len(words_and_tags)):
-		add_to_emissions(emission_matrix, words_and_tags[i][1], words_and_tags[i][0])
-		word_set.add(words_and_tags[i][0])
-
-def calculate_probabilities(matrix):
-	for x in matrix.keys():
-		total = sum(matrix[x].values())
-		for y in matrix[x].keys():
-			matrix[x][y] = math.log(matrix[x][y] / total)
-
-def write_model(hmmmodel, matrix):
-	for d in matrix.items():
-		hmmmodel.write(str(d[0]) + '\n')
-		hmmmodel.write(str(d[1]) + '\n')
-	hmmmodel.write('*****\n')
-
-def dptable(V, pathScores, states):
-    print(" ".join(("%10d" % i) for i in range(V.shape[0])))
-    for i, y in enumerate(pathScores.T):
-        print("%.7s: " % states[i])
-        print(" ".join("%.7s" % ("%f" % yy) for yy in y))
-
-if __name__ == '__main__':
-
-	transition_matrix = {}
-	emission_matrix = {}
-	states = {}
-	obs = {}
-	reverse_obs = {}
-	initial = []
-	transitions = []
-	emissions = []
-	word_set = set()
-
-	with codecs.open(sys.argv[1], 'r', 'utf-8') as tagged_file:
-		for line in tagged_file:
-			words_and_tags = split_line(line)
-			update_transitions(words_and_tags, transition_matrix)
-			update_emissions(words_and_tags, emission_matrix, word_set)
-
-	calculate_probabilities(transition_matrix)
-	calculate_probabilities(emission_matrix)
-
-	i = 0
-	for state in transition_matrix.keys():
-		if state != 'q0':
-			states[i] = state
-			i += 1
-
-	for i in xrange(len(states.keys())):
-		if transition_matrix['q0'][states[i]]:
-			initial.append(transition_matrix['q0'][states[i]])
-		else:
-			initial.append(0.0)
-
-	for i in xrange(len(states.keys())):
-		row = [0.0] * len(states.keys())
-		if states[i] in transition_matrix.keys():
-			for j in xrange(len(states.keys())):
-				if states[j] in transition_matrix[states[i]].keys():
-					row[j] = transition_matrix[states[i]][states[j]]
-		transitions.append(row)
-
-	j = 0
-	for word in word_set:
-		obs[j] = word
-		reverse_obs[word] = j
-		row = [0.0] * len(states.keys())
-		for i in xrange(len(states.keys())):
-			if states[i] in emission_matrix.keys() and word in emission_matrix[states[i]].keys():
-				row[i] = emission_matrix[states[i]][word]
-		emissions.append(row)
-
-	p0 = np.array(initial)
-	trans = np.array(transitions)
-	emi = np.array(emissions)
-
-	np_model = HMMNumpy(trans, p0)
-
-	hmmoutput = codecs.open('hmmoutput.txt','w', 'utf-8')
-
-	with codecs.open(sys.argv[2], 'r', 'utf-8') as untagged_file:
-		for line in untagged_file:
-			words = line.strip().split()
-			line = []
-
-			for word in words:
-				if word in word_set:
-					line.append(reverse_obs[word])
-
-			obs_seq = np.array(line)
-			y = emi[obs_seq]
-			np_states, np_scores = np_model.viterbi_decode(y)
-			pathScores = np.array(np.exp(np_scores))
-
-			pathScores = np.array(np.exp(np_scores))
-    		dptable(pathScores, pathScores, states)
-			
+          last[prev] = 1
 
 
-#	hmmmodel = codecs.open('hmmmodel.txt','w', 'utf-8')
-#	write_model(hmmmodel, transition_matrix)
-#	write_model(hmmmodel, emission_matrix)
+
+
+# print transition_dict 
+# print "\n"
+# print trasition_count
+# print "\n"
+# print emission_dict
+# print "\n"
+# print last
+# print "\n"
+
+emission_count = copy.deepcopy(trasition_count)
+
+for t in last:
+  if t in trasition_count:
+     emission_count[t] += last[t]
+
+  else:
+     if t not in emission_count:
+        emission_count[t] = last[t]
+     else:
+        emission_count[t] += last[t]
+
+     
+
+
+# print trasition_count
+# print "\n"
+# print emission_count
+# print "\n"
+
+len_transition_dict = 0
+len_emission_dict = 0
+
+f = open('hmmmodel.txt','wb')
+len_of_trans_count = len(trasition_count)
+# print "\n TRns count "
+# print  len_of_trans_count 
+
+#f.write('Transmition\n')
+
+for prev1 in transition_dict:
+  for tag1 in transition_dict[prev1]:
+    #print prev1, tag1
+    if prev1 not in transition_prob:
+      transition_prob[prev1] = {}
+    transition_prob[prev1][tag1] = (float)((1 + (float)(transition_dict[prev1][tag1])) / ((float)(trasition_count[prev1]) + len_of_trans_count))  #add 1 smoothing
+
+    #(no. of transitions from prev1->tag1)/(no. of prev1 tags)
+    len_transition_dict += 1
+  
+  transition_prob[prev1]["qX"] = 1 / (((float)(trasition_count[prev1]) + len_of_trans_count))
+#print transition_prob
+
+#len_transition_dict = len(transition_dict)
+# print "\n Length of transition dict: " + str(len_transition_dict + len_of_trans_count)
+
+f.write(str(len_transition_dict + len_of_trans_count) + "\n")
+
+for prev1 in transition_dict:
+  for tag1 in transition_dict[prev1]:
+    s = prev1 +'\t'+ tag1 + '\t' + str(transition_prob[prev1][tag1]);
+    f.write(s+"\n")
+  s = prev1 + '\t'+ "qX" + '\t' + str(transition_prob[prev1]["qX"]);
+  f.write(s+"\n")
+
+
+
+#f.write('Emission\n')
+
+for tag2 in emission_dict:
+  for word2 in emission_dict[tag2]:
+    #print prev1, tag1
+    if tag2 not in emission_prob:
+      emission_prob[tag2] = {}
+    emission_prob[tag2][word2] = (float)((float)(emission_dict[tag2][word2]) / emission_count[tag2])
+    #(no. of transitions from prev1->tag1)/(no. of prev1 tags)
+    len_emission_dict += 1
+    
+                    
+#len_emission_dict = len(emission_dict)
+# print "\n Length of emission dict: " + str(len_emission_dict)
+
+f.write(str(len_emission_dict) + "\n")
+
+for tag2 in emission_dict:
+  for word2 in emission_dict[tag2]:
+    s = tag2 +'\t'+ word2 + '\t' + str(emission_prob[tag2][word2]);
+    f.write(s+"\n")
+
+for key, value in possible_tags_dict.iteritems():
+    f.write(key+' ')
+    for all_tags in value:
+        f.write(all_tags+' ')
+    f.write('\n')
+
+
+
+# print transition_prob
+# print emission_prob
+# print possible_tags_dict
